ec0837f053dc1b9eb229f8b2b40d964d26012847
ARYAN YADAV <merndeveloperaryan@gmail.com>
2024-11-01 11:09:19 +0530

    Now I am going to start my dsa journey using java

---
+++ FundamentalsOfProgramming/02CommentsVariables/UsingVariable.java
@@ -0,0 +1,7 @@
+public class UsingVariable {
+    public static void main(String[] args) {
+        int age = 19;
+        String name = "Aryan";
+        System.out.println("My name is " + name + " and my age is " + age);
+    }
+}
---
+++ FundamentalsOfProgramming/04DataTypes/DataTypes.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class DataTypes {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a = sc.nextInt();
+        System.out.println("Enter second number");
+        int b = sc.nextInt();
+
+        System.out.println("The sum of " + a + " and " + b + " is " + (a + b));
+        sc.close();
+    }
+}
